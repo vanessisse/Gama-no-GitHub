930b4a259c60f3c9caaee5acee1f0483cce68f0a
vanessisse <vanessasillos@gmail.com>
2021-07-08 18:19:12 -0300

    Tipos de operadores

---
+++ Aulas-de-JavaScript/operadores.js
@@ -0,0 +1,67 @@
+/*
+
+Operadores aritméticos:
+
+Adição: +
+Subtração: -
+Multiplicação: *
+Divisão: /
+Módulo ou resto de uma divisão: %
+Incremento: ++
+Decremento: --
+
+*/
+
+var sumTwoNumbers = 1 + 6
+console.log(sumTwoNumbers)
+
+var subTwoNumbers = 4 - 2
+console.log(sumTwoNumbers)
+
+var multTwoNumbers = 5 * 2
+console.log(multTwoNumbers)
+
+var divTwoNumbers = 4 / 2
+console.log(divTwoNumbers)
+
+console.log(8 % 4)
+
+var num = 1;
+console.log(++num) //num = num + 1
+
+var num = 2;
+console.log(--num) //num = num - 1
+
+/*
+
+Operadores de atribuição:
+
+Atribui-se usando o símbolo "="
+
+*/
+
+var x = 4
+var y = 2
+console.log(x += y) //x = x + y
+console.log(x -= y)
+console.log(x *= y)
+console.log(x /= y)
+console.log(x %= y)
+
+/*
+
+Operadores de comparação:
+
+Igual: ==
+Estritamente igual: ===
+Diferente: !=
+Menor que ou igual: < ou <=
+Maior que ou igual: > ou >=
+
+Operadores lógicos:
+
+AND — &&: retornará true se ambos os operandos forem true.
+OR — ||: retornará true se um ou ambos os operandos for true.
+!: inverte o significado de seu operando (negação).
+
+*/
