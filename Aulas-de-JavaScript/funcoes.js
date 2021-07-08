0a8ce23859873abef2002e14ca35bc8991af3d9c
vanessisse <vanessasillos@gmail.com>
2021-07-08 18:24:40 -0300

    Exemplos de funções

---
+++ Aulas-de-JavaScript/funcoes.js
@@ -0,0 +1,15 @@
+// Exemplo 1:
+
+function sayHello(name, lastName) {
+    console.log("Olá, " + name + " " + lastName)
+}
+
+sayHello("Minnie", "Mouse");
+
+// Exemplo 2:
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(40, 60))
