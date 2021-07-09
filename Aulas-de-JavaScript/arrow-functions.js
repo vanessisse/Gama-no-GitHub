f32f8b34f82d9c530aef4d77faa6a4b5088bf144
vanessisse <vanessasillos@gmail.com>
2021-07-08 21:26:47 -0300

    Utilizando arrow functions

---
+++ Aulas-de-JavaScript/arrow-functions.js
@@ -0,0 +1,15 @@
+//function
+
+function soma(a, b) {
+    return a + b
+}
+
+console.log(soma(4, 2)) //6
+
+//arrow function
+
+const soma = (num1, num2) => num1 + num2
+
+const sayHello = name => `Hello ${name}`
+
+console.log(sayHello(`Kitty`))
